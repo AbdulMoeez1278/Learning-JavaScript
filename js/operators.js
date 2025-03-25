1adce59dc3227f9c346b703021dc586394a9c450
AbdulMoeez1278 <abdulmoeez1278@gmail.com>
2025-03-25 21:01:30 +0500

    08_Operators-in-JS
    JS file about the operators in javascript.

---
+++ js/operators.js
@@ -0,0 +1,49 @@
+// OPERATORS
+
+// 1: Arithmetic Operators
+let a = 20;
+let b = 10;
+
+// console.log("a + b = ", a+b); // addition
+// console.log("a - b = ", a-b); // subtraction
+// console.log("a * b = ", a*b); // multiplication
+// console.log("a / b = ", a/b); // division
+// console.log("a % b = ", a%b); // modulus
+// console.log("a ** b = ", a**b); // exponentiation
+// console.log("a++ = ", a++); // increment
+// console.log("a-- = ", a--); // decrement
+
+// 2: Assignment Operators
+let x = 10;
+let y = 10;
+
+x += 5; // plus equals too
+y -= 5; // minus equals too
+a *= 5; // multiple equals too
+b /= 5; // division equals too
+x %= 5; // modulus equals too
+y **= 10; // exponentiation equals too
+
+// 3: Comparison Operators
+let comp1 = 5;
+let comp2 = 10;
+let comp3 = "20";
+
+// console.log("comp1 == comp2 = ", comp1 == comp2); // equals to
+// console.log("comp1 != comp2 = ", comp1 != comp2); // not equals to
+// console.log("comp1 === comp3 = ", comp1 === comp3); // equal value or datatype
+// console.log("comp1 !== comp3 = ", comp1 !== comp3); // not equal value or datatype
+// console.log("comp1 > comp2 = ", comp1 > comp2); // greater than
+// console.log("comp1 < comp2 = ", comp1 < comp2); // less than
+// console.log("comp1 >= comp2 = ", comp1 >= comp2); // greater than or equals to
+// console.log("comp1 <= comp2 = ", comp1 <= comp2); // less than or equals to
+// console.log("comp1 ? comp2 = ", comp1 ? comp2); // ternary operator
+
+// 4: Logical Operators
+let m = 5;
+let n = 10;
+
+// console.log(m>n && m==5); // AND operator ---> both conditions true then it prints true otherwise false
+// console.log(n>m || n==5); // OR operator ---> if one of the condition is true it prints true
+// console.log(!false); // NOT operator ---> output results is true
+// console.log(!true); // NOT operator ---> output result is false
