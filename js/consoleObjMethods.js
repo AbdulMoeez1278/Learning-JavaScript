6d407c4d054ac6e3e7fb8326d51b2d38c5498823
AbdulMoeez1278 <abdulmoeez1278@gmail.com>
2025-03-27 00:20:56 +0500

    13_Console-Object-Methods-in-JS

---
+++ js/consoleObjMethods.js
@@ -0,0 +1,36 @@
+// CONSOLE OBJECT METHODS
+
+// console.log(log); ---> all console methods you can see using this command
+// console.log(console); ---> all console methods you can see using this command
+
+// 1: log() ---> outputs a message to the console
+// console.log("Abdul Moeez");
+
+// 2:error() ---> used for errors
+// console.error(err);
+// console.error("This is an error");
+
+// 3:asserts() ---> used to assert a condition | statement
+// console.assert("err" != false);
+// console.assert("err" == false);
+// console.log(20 > 50);
+// console.assert(20 > 10);
+
+// 4: clear() ---> clears the console
+// console.clear();
+
+// 5: table() ---> displays a tabular data
+let obj = {
+  a: 1,
+  b: 2,
+  c: 3,
+};
+// console.table(obj);
+
+// 6: warn() ---> used for warnings
+// console.warn(warn);
+// console.warn("Please don't drink alcohal");
+
+// 7: info() ---> used for special information
+// console.info(info);
+// console.info("Hey, this is an important info");
