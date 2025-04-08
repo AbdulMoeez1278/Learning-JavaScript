7ef118ae7819709efe61cc96bf67287d8c1e4e0a
AbdulMoeez1278 <abdulmoeez1278@gmail.com>
2025-04-09 01:33:49 +0500

    22_Changing-HTML-Classes-in-JS

---
+++ js/changingHtmlClasses.js
@@ -0,0 +1,13 @@
+// changing HTML classes | className | classList
+
+// className
+first.className = "text-black red";
+
+// classList
+first.classList.add("red"); // add a class
+
+first.classList.remove("red"); // removes a class
+
+first.classList.toggle("red"); // adds the class if it does'nt exist, otherwise removes it
+
+first.classList.contain("red"); // checks for the given class returns true | false
