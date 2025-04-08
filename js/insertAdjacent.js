bfa51b227bf10adef292b224d2aa1d1ca29cd955
AbdulMoeez1278 <abdulmoeez1278@gmail.com>
2025-04-09 00:40:33 +0500

    21_insertAdjacents-in-JS
    JS file about the insertAdjacentHTML | insertAdjacentText | insertAdjacentElement in Javascript.

--- js/insertAdjacent.js
+++ js/insertAdjacent.js
@@ -1,2 +1,15 @@
-// insertAdjacentHTML | insertAdjacentElement | insertAdjacentText 
+// insertAdjacentHTML | insertAdjacentElement | insertAdjacentText
 
+// insertAdjacentHTML
+
+// 1: beforebegin Method // Insert HTML immediately before Element
+first.insertAdjacentHTML("beforebegin", '<div class="test">beforebegin</div>');
+
+// 2: afterbegin Method // Insert HTML into Element at the begining
+first.insertAdjacentHTML("afterbegin", '<div class="test">afterbegin</div>');
+
+// 3: beforeend Method // Insert HTML into Element at the end
+first.insertAdjacentHTML("beforeend", '<div class="test">beforeend</div>');
+
+// 4: afterend Method // Insert HTML immediately after Element
+first.insertAdjacentHTML("afterend", '<div class="test">afterend</div>');
