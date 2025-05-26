a4a73eb057faa0b0c2248b9c7b5f86a4da2d708a
AbdulMoeez1278 <abdulmoeez1278@gmail.com>
2025-05-27 00:14:11 +0500

    Update Events.js

--- js/events.js
+++ js/events.js
@@ -18,9 +18,9 @@ const button = document.querySelector("button");
 // }); // when mouse leave out the button then this event occurs | triggered
 
 // dblclick Event
-button.addEventListener("dblclick", function () {
-  console.log(button.innerHTML);
-}); // when clicked on the button double times then this event occurs | triggered
+// button.addEventListener("dblclick", function () {
+//   console.log(button.innerHTML);
+// }); // when clicked on the button double times then this event occurs | triggered
 
 // keydown Event
 // button.addEventListener("keydown", function () {
@@ -28,8 +28,62 @@ button.addEventListener("dblclick", function () {
 // }); // when pressed the keydown button then this event occurs | triggered
 
 // keypress Event
-button.addEventListener("keypress", function () {
-  console.log(button);
-  //   console.log(button.innerHtml); // gives the undefined value in the console when using it with the keypress event
-  console.log(button.innerText); // it works properly and gives the accurate value
-}); // when press any button on the keyboard then this event occurs | triggered
+// button.addEventListener("keypress", function () {
+//   console.log(button);
+//   //   console.log(button.innerHtml); // gives the undefined value in the console when using it with the keypress event
+//   console.log(button.innerText); // it works properly and gives the accurate value
+// }); // when press any button on the keyboard then this event occurs | triggered
+
+// Practiced Question
+
+// Build a simple counter that increments whenever a button is clicked
+// let numberCount = document.getElementById("numberCount");
+// let number = 1;
+// function counter() {
+//   for (i = 0; i < number; i++) {
+//     console.log("Counter is running", number);
+//     numberCount.innerText = `${number}`;
+//   }
+//   number++;
+// }
+
+// Build a simple counter that increments whenever a button is clicked
+let numberCount = document.getElementById("numberCount");
+let number = 0;
+
+function counterPlus() {
+  number++;
+  numberCount.innerText = `${number}`;
+  document.getElementById("minusBtn").disabled = number <= 0;
+  //   console.log("Count:", number);
+}
+
+function counterMinus() {
+  number--;
+  if (number <= 0) {
+    number = 0;
+  }
+  numberCount.innerText = `${number}`;
+  document.getElementById("minusBtn").disabled = number <= 0;
+  //   console.log("Count:", number);
+}
+
+counterPlus();
+counterMinus();
+
+// button.addEventListener("click", function (event) {
+//   counterPlus();
+//   event.stopPropagation();
+// });
+
+// button.addEventListener("mouseover", function () {
+//   counterPlus();
+// });
+
+// button.addEventListener("mouseout", function () {
+//   counterMinus();
+// });
+
+// button.addEventListener("keypress", function () {
+//   counterPlus();
+// });
