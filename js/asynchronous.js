80942cb1ae65c8c8438f12a59b0c928de0dfb567
AbdulMoeez1278 <abdulmoeez1278@gmail.com>
2025-06-17 01:51:34 +0500

    Create asynchronous.js

---
+++ js/asynchronous.js
@@ -0,0 +1,51 @@
+// Define three functions
+function firstTask() {
+  console.log("Task 1");
+}
+
+function secondTask() {
+  console.log("Task 2");
+}
+
+function thirdTask() {
+  console.log("Task 3");
+}
+
+// Execute the functions
+firstTask();
+secondTask();
+thirdTask();
+
+// synchronus programming example
+
+// function someLongRunningFunction() {
+//   let start = Date.now();
+//   while (Date.now() - start < 5000) {
+// do nothing
+//   }
+//   return "Hello";
+// }
+
+// console.log("Starting...");
+
+// let result = someLongRunningFunction();
+// console.log(result);
+
+// console.log("...Finishing");
+
+// asynchronous programming
+console.log("Start of script");
+setTimeout(() => {
+  console.log("First timeout completed");
+}, 2000);
+
+console.log("End of script");
+
+//
+// function syncFirstTask() {
+//   console.log("Synchronous first task");
+// }
+
+// function syncSecondTask() {
+//   console.log("Synchronous second task");
+// }
