53c2225848b0586c6097490e1e152e90ff663765
AbdulMoeez1278 <150109036+AbdulMoeez1278@users.noreply.github.com>
2025-06-24 01:58:09 +0500

    Update asyncAwait.js

--- js/asyncAwait.js
+++ js/asyncAwait.js
@@ -186,6 +186,24 @@ let apiData = async () => {
 
 apiData();
 
+// using an IIFE for fetching data through an api
+(async function () {
+  try {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/todos/1"
+    );
+    const json = await response.json();
+    if (json.userId == 1) {
+      json.completed == false;
+    } else {
+      json.completed == true;
+    }
+    console.log(json);
+  } catch (error) {
+    console.log(error);
+  }
+})();
+
 // ...  JS - PRACTICE  ...
 
 // let arr = [
