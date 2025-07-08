1a0e8c830a91c8cf2d6894cbd426be0b50468a1f
AbdulMoeez1278 <150109036+AbdulMoeez1278@users.noreply.github.com>
2025-07-08 18:40:26 +0500

    Update asyncAwait.js

--- js/asyncAwait.js
+++ js/asyncAwait.js
@@ -206,22 +206,22 @@ let apiData = async () => {
 apiData();
 
 // using an IIFE for fetching data through an api
-// (async function () {
-//   try {
-//     const response = await fetch(
-//       "https://jsonplaceholder.typicode.com/todos/1"
-//     );
-//     const json = await response.json();
-//     if (json.userId == 1) {
-//       json.completed == false;
-//     } else {
-//       json.completed == true;
-//     }
-//     console.log(json);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// })();
+(async function () {
+  try {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/todos/1"
+    );
+    const json = await response.json();
+    if (json.userId == 1) {
+      json.completed == false;
+    } else {
+      json.completed == true;
+    }
+    console.log(json);
+  } catch (error) {
+    console.log(error);
+  }
+})();
 
 // async named function using fetch api
 async function fetchData() {
