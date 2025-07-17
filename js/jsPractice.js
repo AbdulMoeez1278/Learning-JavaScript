cf92dead98acf706d21b65bbdd40ef2deb6a2751
AbdulMoeez1278 <150109036+AbdulMoeez1278@users.noreply.github.com>
2025-07-18 01:47:11 +0500

    Update jsPractice.js

--- js/jsPractice.js
+++ js/jsPractice.js
@@ -7,7 +7,8 @@
 let a; // declaration
 let b = 10; // declaration & initialization
 
-// string method : slice() method
+// string methods
+// slice() method - (starting and ending index) - the ending index not included
 let text = "Apple, Banana, Kiwi";
 
 let part1 = text.slice(7); // slice out the rest of parameters
@@ -24,14 +25,69 @@ console.log(part4);
 
 let str = "Apple, Banana, Kiwi";
 
-let part = str.substring(7, 13);
-console.log(part);
+// substring method // same as slice method (starting and ending index) - the ending index not included
+let subStringPart = str.substring(7, 13);
+console.log(subStringPart);
 
 // toUpperCase method
 let text1 = "Hello World!";
 let text2 = text1.toUpperCase();
 console.log(text2);
 
+// toLowerCase method
 let text3 = "HellO WORLD!";
-let text5 = text1.toLowerCase();
-console.log(text5);
+let text4 = text1.toLowerCase();
+console.log(text4);
+
+// trim method
+let strText = "    Hello World!    ";
+console.log(strText);
+
+let strText2 = "Abdul Moeez";
+console.log(strText2);
+
+let textTrim = strText.trim(); // removes white spaces in the text
+console.log(textTrim);
+
+// trimStart method
+let trimStart = strText.trimStart(); // removes white spaces from the start of the text
+console.log(trimStart);
+
+// trimEnd method
+let trimEnd = strText.trimEnd(); // removes white spaces from the end of the text
+console.log(trimEnd);
+
+// repeat method
+let repeatText = strText.repeat(2); // repeats the word according to the value passed to the parameters || returns a new string with a number of copies of a string
+console.log(repeatText);
+
+// length method
+let strLength = strText2.length; // returns the length of the text in numbers
+console.log(strLength);
+
+// Arrays - are object types that are used for storing data collections
+let arr = [];
+console.log(arr);
+
+let arr2 = [10, 20, 30];
+console.log(arr2);
+console.log(arr2[0]);
+console.log(arr2[1]);
+console.log(arr2[2]);
+
+let arr3 = ["HTML", "CSS", "JS"];
+console.log(arr3);
+
+// Accessing Last Array Elements
+let lastItem = arr3[arr3.length - 1];
+console.log("Last Item is: ", lastItem);
+
+// modifying the array elements
+arr3[1] = "Bootstrap"; // element at index 1
+console.log(arr3);
+
+arr3[0] = "XHTML"; // element at index 0
+console.log(arr3);
+
+arr3[2] = "TS"; // element at index 2
+console.log(arr3);
